0dbb71edf9a366a69be2f186662503f71a44be86
mmhamza <mmhamza@tecizeverything.com>
2022-11-07 21:46:56 +0500

    Remove while loop, use array data structure insted of switch, separate business from program

---
+++ Old Phone Challange/OldPhoneKeyPad/OldPhoneKeyPad.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Old_Phone_Challange
+{
+    class OldPhoneKeyPad : IOldPhoneKeyPad
+    {
+        /// <summary>
+        /// This method will take Input Character and Number of time it pressed, and will return respective output
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="count"></param>
+        /// <returns></returns> 
+        public static string GetAlphabet(char input, int count)
+        {
+            string[,] Alphabet = { { "&", "'", "(", "" }, { "A", "B", "C", "" }, { "D", "E", "F", "" }, { "G", "H", "I", "" }, { "J", "K", "L", "" }, { "M", "N", "O", "" }, { "P", "Q", "R", "S" }, { "T", "U", "V", "" }, { "W", "X", "Y", "Z" } };
+            int index = 0;
+            try
+            {
+                index = int.Parse(input.ToString()) - 1;
+            }
+            catch (System.FormatException)
+            {
+                return "";
+            }
+            return Alphabet[index, count - 1];
+
+        }
+
+        /// <summary>
+        ///This will turn any input to OldPhonePad into the correct output
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string OldPhonePad(string input)
+        {
+            if (input.Length == 0)
+            {
+                return "";
+            }
+            string output = "";
+            char previousLetter = input[0];
+            int pressedCount = 0;
+            bool backSpaceFlag = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentLetter = input[i];
+                if (currentLetter == previousLetter && currentLetter != '*')
+                {
+                    if (backSpaceFlag)
+                        backSpaceFlag = false;
+                    pressedCount++;
+                    continue;
+                }
+                else if (currentLetter == '*')
+                {
+                    if (backSpaceFlag)
+                    {
+                        if (output.Length > 0)
+                            output = output.Remove(output.Length - 1);
+                        backSpaceFlag = false;
+                    }
+                    if (i + 1 < input.Length)
+                    {
+                        previousLetter = input[i + 1];
+                        pressedCount = 0;
+                        backSpaceFlag = true;
+                    }
+
+                    continue;
+                }
+                else
+                {
+                    if (previousLetter == '7' || previousLetter == '9') // Letter 7 and 9 has 4 Alphabets
+                    {
+                        if (pressedCount > 4) // This will check if user has pressed Letter more than 4 time, It will cycle through each letter again
+                            pressedCount -= 4;
+                    }
+                    else if (pressedCount > 3)
+                    {
+                        pressedCount -= 3; // This will check if user has Letter more than 3 time, It will cycle through each letter again
+                    }
+                    // This method will get respective Alphabet based on input character and number of time it pressed
+                    output += GetAlphabet(previousLetter, pressedCount);
+                    if (currentLetter == '#')
+                        return output;
+                    previousLetter = currentLetter;
+                    pressedCount = 1;
+                }
+
+            }
+            return output;
+        }
+
+        public string GetOldPhoneKeys(string inputKeys)
+        {
+            return OldPhonePad(inputKeys);
+        }
+
+    }
+}
--- Old Phone Challange/Program.cs
+++ Old Phone Challange/Program.cs
@@ -4,238 +4,27 @@ namespace Old_Phone_Challange
 {
     class Program
     {
-        /// <summary>
-        /// This method will take Input Character and Number of time it pressed, and will return respective output
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="count"></param>
-        /// <returns></returns>
-        public static string GetAlphabet(char input, int count)
-        {
-            switch (input)
-            {
-                case '1':
-                    if (count == 1)
-                    {
-                        return "&";
-                    }
-                    else if (count == 2)
-                    {
-                        return "'";
-                    }
-                    else if (count == 3)
-                    {
-                        return "(";
-                    }
-                    break;
-                case '2':
-                    if (count == 1)
-                    {
-                        return "A";
-                    }
-                    else if (count == 2)
-                    {
-                        return "B";
-                    }
-                    else if (count == 3)
-                    {
-                        return "C";
-                    }
-                    break;
-                case '3':
-                    if (count == 1)
-                    {
-                        return "D";
-                    }
-                    else if (count == 2)
-                    {
-                        return "E";
-                    }
-                    else if (count == 3)
-                    {
-                        return "F";
-                    }
-                    break;
-                case '4':
-                    if (count == 1)
-                    {
-                        return "G";
-                    }
-                    else if (count == 2)
-                    {
-                        return "H";
-                    }
-                    else if (count == 3)
-                    {
-                        return "I";
-                    }
-                    break;
-                case '5':
-                    if (count == 1)
-                    {
-                        return "J";
-                    }
-                    else if (count == 2)
-                    {
-                        return "K";
-                    }
-                    else if (count == 3)
-                    {
-                        return "L";
-                    }
-                    break;
-                case '6':
-                    if (count == 1)
-                    {
-                        return "M";
-                    }
-                    else if (count == 2)
-                    {
-                        return "N";
-                    }
-                    else if (count == 3)
-                    {
-                        return "O";
-                    }
-                    break;
-                case '7':
-                    if (count == 1)
-                    {
-                        return "P";
-                    }
-                    else if (count == 2)
-                    {
-                        return "Q";
-                    }
-                    else if (count == 3)
-                    {
-                        return "R";
-                    }
-                    else if (count == 4)
-                    {
-                        return "S";
-                    }
-                    break;
-                case '8':
-                    if (count == 1)
-                    {
-                        return "T";
-                    }
-                    else if (count == 2)
-                    {
-                        return "U";
-                    }
-                    else if (count == 3)
-                    {
-                        return "V";
-                    }
-                    break;
-                case '9':
-                    if (count == 1)
-                    {
-                        return "W";
-                    }
-                    else if (count == 2)
-                    {
-                        return "X";
-                    }
-                    else if (count == 3)
-                    {
-                        return "Y";
-                    }
-                    else if (count == 3)
-                    {
-                        return "Z";
-                    }
-                    break;
-            }
-            return "";
-        }
-
-        /// <summary>
-        ///This will turn any input to OldPhonePad into the correct output
-        /// </summary>
-        /// <param name="input"></param>
-        /// <returns></returns>
-        public static string OldPhonePad(string input)
-        {
-            if (input.Length == 0)
-            {
-                return "";
-            }
-            string output = "";
-            char previousLetter = input[0];
-            int pressedCount = 0;
-            for (int i = 0; i < input.Length; i++)
-            {
-                char currentLetter = input[i];
-                if (currentLetter == previousLetter)
-                {
-                    pressedCount++;
-                    continue;
-                }
-                else if(currentLetter == '*')
-                {
-                    while (i  < input.Length) // Just in case * is a last letter, and there is no '#', This condition is just for safe side to avoid exception
-                    {
-                        // This loop wil work unless all backspace character in sequence are checked.
-                        if (i + 1 < input.Length)
-                        {
-                            if (input[i + 1] == '*') // If next character is also a back space.
-                            {
-                                output = output.Remove(output.Length - 1, 1);
-                                i++;
-                            }
-                            else
-                            {
-                                i++;
-                                previousLetter = input[i];
-                                pressedCount = 1;
-                                break;
-                            }
-                        }
-                    }
-                    continue;
-                }
-                else 
-                {
-                    if (previousLetter == '7' || previousLetter == '9') // Letter 7 and 9 has 4 Alphabets
-                    {
-                        if (pressedCount > 4) // This will check if user has pressed Letter more than 4 time, It will cycle through each letter again
-                            pressedCount -= 4;
-                    }
-                    else if (pressedCount > 3)
-                    {
-                        pressedCount -= 3; // This will check if user has Letter more than 3 time, It will cycle through each letter again
-                    }
-                    // This method will get respective Alphabet based on input character and number of time it pressed
-                    output += GetAlphabet(previousLetter, pressedCount);
-                    if (currentLetter == '#')
-                        return output;
-                    previousLetter = currentLetter;
-                    pressedCount = 1;
-                }
-
-            }
-            return output;
-        }
+        
+       
 
         static void Main(string[] args)
         {
-            //Console.WriteLine(OldPhonePad("#"));
-            //Console.WriteLine(OldPhonePad(""));
-            //Console.WriteLine(OldPhonePad("*"));
-            //Console.WriteLine(OldPhonePad("*7*"));
-            //Console.WriteLine(OldPhonePad("7#"));
-            //Console.WriteLine(OldPhonePad("7*#"));
-            //Console.WriteLine(OldPhonePad("*#"));
-            //Console.WriteLine(OldPhonePad("33#"));
-            //Console.WriteLine(OldPhonePad("227*#"));
-            //Console.WriteLine(OldPhonePad("4433555 555666#"));
-            Console.WriteLine(OldPhonePad("8 88777444666*664#"));
-          //  Console.WriteLine(OldPhonePad("8 887774446667**664#"));
 
-          
+            IOldPhoneKeyPad phone = new OldPhoneKeyPad();
+            Console.WriteLine(phone.GetOldPhoneKeys("#"));
+            Console.WriteLine(phone.GetOldPhoneKeys(""));
+            Console.WriteLine(phone.GetOldPhoneKeys("*"));
+            Console.WriteLine(phone.GetOldPhoneKeys("*7*"));
+            Console.WriteLine(phone.GetOldPhoneKeys("7#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("7*#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("*#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("33#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("227*#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("4433555 555666#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("8 88777444666*664#"));
+            Console.WriteLine(phone.GetOldPhoneKeys("8 887774446667**664#"));
+
+
         }
     }
 }
